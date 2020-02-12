458cbe4f131d3507950d31c4473b68812db0e746
Fernando1809 <ferandre1718.frg@gmail.com>
2020-02-12 13:48:49 -0600

    hola que hace

---
+++ Estadistica/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Estadistica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblserie = new System.Windows.Forms.Label();
+            this.txtrespuesta = new System.Windows.Forms.TextBox();
+            this.lblrespuesta_serie = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblserie
+            // 
+            this.lblserie.AutoSize = true;
+            this.lblserie.Location = new System.Drawing.Point(12, 27);
+            this.lblserie.Name = "lblserie";
+            this.lblserie.Size = new System.Drawing.Size(39, 13);
+            this.lblserie.TabIndex = 0;
+            this.lblserie.Text = "SERIE";
+            // 
+            // txtrespuesta
+            // 
+            this.txtrespuesta.Location = new System.Drawing.Point(71, 27);
+            this.txtrespuesta.Name = "txtrespuesta";
+            this.txtrespuesta.Size = new System.Drawing.Size(178, 20);
+            this.txtrespuesta.TabIndex = 1;
+            // 
+            // lblrespuesta_serie
+            // 
+            this.lblrespuesta_serie.AutoSize = true;
+            this.lblrespuesta_serie.Location = new System.Drawing.Point(12, 80);
+            this.lblrespuesta_serie.Name = "lblrespuesta_serie";
+            this.lblrespuesta_serie.Size = new System.Drawing.Size(14, 13);
+            this.lblrespuesta_serie.TabIndex = 2;
+            this.lblrespuesta_serie.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular X";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblrespuesta_serie);
+            this.Controls.Add(this.txtrespuesta);
+            this.Controls.Add(this.lblserie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblserie;
+        private System.Windows.Forms.TextBox txtrespuesta;
+        private System.Windows.Forms.Label lblrespuesta_serie;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
